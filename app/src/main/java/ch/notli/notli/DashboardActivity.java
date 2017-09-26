e3cc2251f4e2544ab4029387cf614baa3a533620
zkappa <andrin.kappeler2000@gmail.com>
2017-09-26 11:42:14 +0200

    added Chart Library

--- app/src/main/java/ch/notli/notli/DashboardActivity.java
+++ app/src/main/java/ch/notli/notli/DashboardActivity.java
@@ -1,12 +1,9 @@
 package ch.notli.notli;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -14,15 +11,6 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
