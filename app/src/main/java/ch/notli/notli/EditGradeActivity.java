e3695fe692aed52217f62c188935af5afde651bb
Toby <tobias.fatzer@bluewin.ch>
2017-09-26 14:00:00 +0200

    EditGradActivity (first design)

---
+++ app/src/main/java/ch/notli/notli/EditGradeActivity.java
@@ -0,0 +1,13 @@
+package ch.notli.notli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditGradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_grade);
+    }
+}
