1aa36d458d489deca9f099d277930b1429905d75
JonasGredig <jongredig@gmail.com>
2017-10-03 10:42:37 +0200

    added categories in SettingsActivity

--- app/src/main/java/ch/notli/notli/DashboardActivity.java
+++ app/src/main/java/ch/notli/notli/DashboardActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.github.mikephil.charting.charts.BarChart;
-
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
