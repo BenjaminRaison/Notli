f8664ecdf575839486f5a2b39c7c4c140732719f
Toby <tobias.fatzer@bluewin.ch>
2017-10-03 09:25:02 +0200

    Reboot AddGradeActivity

---
+++ app/src/main/java/ch/notli/notli/AddGradeActivity.java
@@ -0,0 +1,17 @@
+package ch.notli.notli;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddGradeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_grade);
+
+
+    }
+}
