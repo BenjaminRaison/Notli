6c2b44c5572b778a6d18b61f3178a6ae84984b4e
Toby <tobias.fatzer@bluewin.ch>
2017-10-03 13:25:55 +0200

    No Crashes with CalenderFrame.

--- app/src/main/java/ch/notli/notli/AddEditGradeActivity.java
+++ app/src/main/java/ch/notli/notli/AddEditGradeActivity.java
@@ -1,9 +1,19 @@
 package ch.notli.notli;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
-public class AddEditGradeActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
+
+public class AddEditGradeActivity extends AppCompatActivity{
 
 
 
@@ -11,7 +21,12 @@ public class AddEditGradeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_grade);
+    }
 
 
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 }
+
