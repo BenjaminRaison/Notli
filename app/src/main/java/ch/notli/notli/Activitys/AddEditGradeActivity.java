52bbbcaaaedbdbe124266f513c4b34cc404e379e
Toby <tobias.fatzer@bluewin.ch>
2017-11-07 07:27:46 +0100

    toadys date gets written in dateText

--- app/src/main/java/ch/notli/notli/Activitys/AddEditGradeActivity.java
+++ app/src/main/java/ch/notli/notli/Activitys/AddEditGradeActivity.java
@@ -1,26 +1,58 @@
 package ch.notli.notli.Activitys;
 
+import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import ch.notli.notli.R;
 
-public class AddEditGradeActivity extends AppCompatActivity{
+public class AddEditGradeActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
-
+    private EditText dateText;
+    private Calendar calendar = Calendar.getInstance();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_grade);
+        dateText = findViewById(R.id.dateEditText);
+        updateDateField();
+        dateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddEditGradeActivity.this.showDatePickerDialog(v);
+            }
+        });
     }
 
 
     public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getFragmentManager(), "datePicker");
+        new DatePickerDialog(this, this, calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        calendar.clear();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        updateDateField();
+    }
+
+    public void updateDateField(){
+        dateText.setText(SimpleDateFormat.getDateInstance().format(calendar.getTime()));
     }
 }
 
--- app/src/main/java/ch/notli/notli/Activitys/DatePickerFragment.java
+++ app/src/main/java/ch/notli/notli/Activitys/DatePickerFragment.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
@@ -15,17 +16,27 @@ import java.util.Calendar;
 public class DatePickerFragment extends DialogFragment
         implements DatePickerDialog.OnDateSetListener {
 
+    private int year;
+    private int month;
+    private int day;
+    private View v;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Calendar calender = Calendar.getInstance();
-        int year = calender.get(Calendar.YEAR);
-        int month = calender.get(Calendar.MONTH);
-        int day = calender.get(Calendar.DAY_OF_MONTH);
+        this.year = calender.get(Calendar.YEAR);
+        this.month = calender.get(Calendar.MONTH);
+        this.day = calender.get(Calendar.DAY_OF_MONTH);
 
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
+
     public void onDateSet(DatePicker view, int year, int month, int day) {
+        this.year = year;
+        this.day = day;
+        this.month = month;
+
 
     }
 }
